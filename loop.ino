f71b356480a1bc3d0798c6cbf15d6957f55f1655
KenwoodFox <31870999+KenwoodFox@users.noreply.github.com>
2018-06-06 16:11:53 -0400

    Moved up a dir

--- loop.ino
+++ loop.ino
@@ -1,19 +1,19 @@
 void loop() 
 {
-  cls();
+  cls(); //clear
 
   blue();
   lcd.print("    FRC 1721    ");
   lcd.print("   Drive Team   ");
   delay(frame); //Comment out for testing
-  cls();
+  cls(); //first screen
 
   lcd.print("  Thinking In   ");
   lcd.print("   Progress...  ");
   delay(frame);
-  cls();
+  cls(); //second screen
 
-  lcd.print("1v1 me m8, ur   ");
+  lcd.print(" 1v1 me m8, ur  ");
   lcd.print("bad at minecraft");
   delay(10);
   while(load <= 32)
@@ -23,6 +23,6 @@ void loop()
     red();
     delay(200);
     load++;
-  }
+  } //third screen
 }
 
